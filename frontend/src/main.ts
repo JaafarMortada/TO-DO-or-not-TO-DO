45c6ab8f103b6c793b49c32f2f25696eeb1233d5
Jaafar Mortada <jaafar1899@outlook.com>
2023-10-20 22:21:51 +0300

    Frontend <> Updating main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,25 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from "./router"
 
-createApp(App).mount('#app')
+import { createPinia } from 'pinia';
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+const pinia = createPinia();
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+
+  .use(vuetify)
+  .mount('#app');
+
