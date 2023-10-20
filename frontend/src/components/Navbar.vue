41cc93dd6d9e4044bc8e7f0c4bcb46cb339db1c7
Jaafar Mortada <jaafar1899@outlook.com>
2023-10-20 22:20:02 +0300

    Frontend <> Updating Navbar Styles

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex gap-10 border-b-2 border-gray-500 h-16 items-center justify-between m-0 px-10 drop-shadow-xl">
+    <div class="flex gap-10 border-b-2 bg-gray-300 border-gray-500 h-16 items-center justify-between m-0 px-10 drop-shadow-xl">
         <router-link class="font-black text-gray-700" to="/">TODO or not TODO</router-link>
         <div class="flex gap-10">
             <router-link class="m-0 p-0 text-gray-700 font-black" to="/login">Login</router-link>
-            <router-link class="m-0 p-0 text-gray-700 font-black" to="/todo-list">TodoList</router-link>
+            <!-- <router-link class="m-0 p-0 text-gray-700 font-black" to="/todo-list">TodoList</router-link> -->
         </div>
 
     </div>
