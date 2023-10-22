61ae8966a2bbb8d02c460e283bb9da69ed6ee157
Jaafar Mortada <jaafar1899@outlook.com>
2023-10-22 22:15:47 +0300

    Frontend <> Adding getTodos Request

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,23 +1,41 @@
 import { defineStore } from 'pinia';
 import { Todo } from '../types';
-import todos from '../api/todos';
+import { sendRequest } from '../utilities/request';
 
 interface State {
     all: Todo[];
     isLoading: boolean;
 }
 
-export const useTodosStore = defineStore('todos',{
+export const useTodosStore = defineStore('todos', {
     state: (): State => ({
         all: [],
         isLoading: true,
-      }),
-    
+    }),
+
     actions: {
         async fetchTodos() {
             try {
-                const todoList = await todos.getTodos();
-                this.all = todoList;
+                const response = await sendRequest({
+                    method: "get",
+                    includeHeaders: true,
+                    route: "/todo/get-todos",
+                    body: null
+                })
+
+                this.all = []
+
+                const userTodos = response.todos
+
+                userTodos.forEach((todo: any) => {
+                    this.addNewTodo({
+                        text: todo.text,
+                        time: todo.time,
+                        id: todo._id,
+                        done: todo.done
+                    })
+                });
+            
             } catch (error) {
                 console.error('Error fetching todos:', error);
             } finally {
@@ -25,19 +43,22 @@ export const useTodosStore = defineStore('todos',{
             }
         },
 
-        async addNewTodo(todo: Todo){
+        addNewTodo(todo: Todo) {
             try {
-                await todos.addTodo()
                 this.all.push(todo)
             } catch (error) {
                 console.error('Error adding todo:', error);
-            } 
+            }
         },
-    
+
+        resetStore() {
+            this.all = []
+        },
+
         async setDone(id: number) {
             const todo = this.all.find((todo: Todo) => todo.id === id);
             if (todo) {
-              todo.done = true
+                todo.done = true
             }
         },
     },
