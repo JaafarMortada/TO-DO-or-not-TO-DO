a089491a42d94abd9cd0920c36651001639d334c
Jaafar Mortada <jaafar1899@outlook.com>
2023-10-22 22:15:57 +0300

    Frontend <> Adding addTodo Request

--- frontend/src/components/AddTodo.vue
+++ frontend/src/components/AddTodo.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        <v-textarea v-model="newTodoText" label="Add a new TODO" variant="outlined"
-            class="!max-w-[500px] !min-w-[500px]"></v-textarea>
+        <v-textarea v-model="newTodoText" label="Add a new TODO" class="!max-w-[500px] !min-w-[500px]"></v-textarea>
         <VueDatePicker v-model="date" :dark=true utc class="mb-5" />
         <v-btn @click="AddTodo" density="default" class="w-full !bg-teal-900 !text-gray-300">Add
             TODO</v-btn>
@@ -10,6 +9,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { sendRequest } from '../utilities/request';
 
 export default defineComponent({
     name: "AddTodo",
@@ -29,12 +29,24 @@ const store = useTodosStore();
 const newTodoText = ref('');
 const date = ref<Date>(new Date);
 
+
+
 const AddTodo = async () => {
-    await store.addNewTodo({
+    const response = await sendRequest({
+        method: "POST",
+        includeHeaders: true,
+        route: "/todo/add-todo",
+        body: {
+            text: newTodoText.value,
+            time: date.value
+        },
+    })
+
+    if (response.message === "Todo created successfully") store.addNewTodo({
         text: newTodoText.value,
         time: date.value,
         done: false,
-        id: store.all.length + 1
+        id: response._id
     })
     newTodoText.value = ''
     date.value = new Date
