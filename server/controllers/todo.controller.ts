87b9aa2e39e5e0ee9f84d0b33a3e717157749a23
Jaafar Mortada <jaafar1899@outlook.com>
2023-10-22 22:14:49 +0300

    Backend <> Adding getTodos API

--- server/controllers/todo.controller.ts
+++ server/controllers/todo.controller.ts
@@ -4,34 +4,58 @@ import { user } from "../models/user.model"
 import { Request, Response, NextFunction } from 'express';
 
 const addTodo = async (req: AuthRequest, res: Response, next: NextFunction) => {
-    try {
-        const reqUser = req.user;
-        const body = req.body;
-        
-        const loggedUser = await user.findById(reqUser._id)
+	try {
+		const reqUser = req.user;
+		const body = req.body;
 
-        if (!loggedUser) return res.status(404).json({ message: 'User not found' })
+		const loggedUser = await user.findById(reqUser._id)
 
-        const newTodo = new todo({
-            text: body.text,
-            done: false,
-            time: body.time,
-            user: loggedUser._id,
+		if (!loggedUser) return res.status(404).json({ message: 'User not found' })
 
-        })
+		const newTodo = new todo({
+			text: body.text,
+			done: false,
+			time: body.time,
+			user: loggedUser._id,
 
-        loggedUser.todos.push(newTodo)
+		})
 
-        await newTodo.save()
-        await loggedUser.save()
+		loggedUser.todos.push(newTodo)
 
-        res.status(201).json({ message: 'Todo created successfully' })
+		await newTodo.save()
+		await loggedUser.save()
 
-      } catch (error) {
-        next(error);
-      }
-    };
+		res.status(201).json({ 
+			message: 'Todo created successfully',
+			_id: newTodo._id
+		})
+
+	} catch (error) {
+		next(error);
+	}
+};
+
+const getTodos = async (req: AuthRequest, res: Response, next: NextFunction) => {
+	try {
+		const reqUser = req.user;
+		
+		const loggedUser = ((await user.findOne({ email: reqUser.email }).populate("todos").select('+password')))
+
+
+		if (!loggedUser) return res.status(404).json({ message: 'Unauthorized' })
+
+		res.status(201).json({
+			message: 'success-refresh-store',
+			todos: loggedUser.todos
+		})
+
+	} catch (error) {
+		next(error);
+	}
+}
 
 export {
-    addTodo,
+	addTodo,
+	getTodos,
+
 }
--- server/routes/todo.route.ts
+++ server/routes/todo.route.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { addTodo } from '../controllers/todo.controller';
+import { addTodo, getTodos } from '../controllers/todo.controller';
 import authMiddleware from '../middlewares/auth.middleware';
 
 export default (router: express.Router) => {
   router.post('/todo/add-todo', authMiddleware, addTodo);
+  router.get('/todo/get-todos', authMiddleware, getTodos);
 };
