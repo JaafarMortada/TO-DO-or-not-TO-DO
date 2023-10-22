5ba94c861e5d903348a038e59ae5d9f4f46dcd65
Jaafar Mortada <jaafar1899@outlook.com>
2023-10-22 17:13:07 +0300

    Backend <> Updating index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import { envConfig } from './configs/env.config.ts';
+import mongoDBConnection from './configs/mongoDB.connection.ts';
+import router from './routes';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', router());
 app.use(express.json())
 app.use(cors());
+app.use('/', router());
 
-app.listen(8000, () => { 
-  console.log("server is running on port: ", 8000)
+app.listen(envConfig.PORT || 8000, async () => { 
+  await mongoDBConnection();
+  console.log("server is running on port: ", envConfig.PORT || 8000)
 })
