9b11d8c7a346f734d9ff4a697db0cfcd0dc77784
Jaafar Mortada <jaafar1899@outlook.com>
2023-10-22 19:55:57 +0300

    Backend <> Adding CORS Middleware

--- server/index.ts
+++ server/index.ts
@@ -4,14 +4,16 @@ import bodyParser from 'body-parser';
 import { envConfig } from './configs/env.config.ts';
 import mongoDBConnection from './configs/mongoDB.connection.ts';
 import router from './routes';
-
+import { setCorsHeaders } from './middlewares/cors.middleware.ts';
 const app = express();
+
+app.use(setCorsHeaders);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', router());
 app.use(express.json())
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:5173'], }));
 app.use('/', router());
 
 app.listen(envConfig.PORT || 8000, async () => { 
